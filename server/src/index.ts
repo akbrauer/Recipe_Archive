698f2515d61e8ec998d5c07d8c8930cc079b13ed
Avery Brauer <averybrauer@gmail.com>
2025-06-16 13:46:19 +0100

    Prepping api deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,4 @@
-import express, { Express, Request, Response, NextFunction } from 'express';
-import cors from "cors";
+import express, { Express, Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import apiRouter from "./routes/api";
 dotenv.config();
@@ -9,29 +8,28 @@ const port = Number(process.env.PORT) || 3000;
 
 const app: Express = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Allow CORS Middleware (Replace with vite proxy config)
-// app.use((req, res, next) => {
-//     res.set('Access-Control-Allow-Origin', "http://localhost:5173");
-//     return next();
-// })
+// Allow CORS Middleware (Replace with vite proxy config)
+app.use((req, res, next) => {
+	res.set("Access-Control-Allow-Origin", "*");
+	return next();
+});
 
 //Logging Middleware
 app.use((req: Request, res: Response, next: NextFunction) => {
-    console.log('--------------------------------');
-    console.log(`Request: ${req.method} ${req.url}`);
-    return next();
+	console.log("--------------------------------");
+	console.log(`Request: ${req.method} ${req.url}`);
+	return next();
 });
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello Typescript Express!');
-})
+app.get("/", (req: Request, res: Response) => {
+	res.send("Hello Typescript Express!");
+});
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
-app.listen(port, host, () => {
-    console.log(`[server]: Server is listening on ${host}:${port}`);
+app.listen(port, () => {
+	console.log(`[server]: Server is listening on ${port}`);
 });
